a8c0cdaafd9a0fad7fad823263fedca02996cdb5
Christina Unger <christina@mercury.ai>
2019-12-03 06:34:37 +0100

    [tsunami] documentation for discrete approximation

---
+++ tsunami/README.md
@@ -0,0 +1,1 @@
+Tsunami simulation, following [Milan Cucic: Modern Fortran. Building efficient parallel applications](https://www.manning.com/books/modern-fortran).
--- tsunami/tsunami.f90
+++ tsunami/tsunami.f90
@@ -25,23 +25,44 @@ program tsunami
   if (v  <= 0) stop 'background flow speed v must be > 0'
 
   ! Initiating the blob as a Gaussian shape.
-  
+
   do concurrent (x = 1:grid_size)
     h(x) = exp(-xdecay * (x - xcenter)**2)
   end do
 
   print *, 0, h
 
+  ! Discrete approximation of the advection equation with finite differences:
+  !
+  !    dh/dt + v*(dh/dt) = 0
+  !
+  ! Which, spelled out, corresponds to:
+  !
+  !    (h(x,t+1) - h(x,t))/dt + v*(h(x,t) - h(x-1,t))/dx = 0
+  !
+  ! Solving for the water height at the next time step, this gives:
+  !
+  !    h(x,t+1) = h(x,t) + v*(h(x,t) - h(x-1,t))/dx * dt
+  !
+  ! Where h(x,t) is the water height at the current time step,
+  ! (h(x,t) - h(x-1,t))/dx is the gradient, i.e. the rate of change in space,
+  ! multiplied by the velocity v, giving the rate of change in time.
+  !
+  ! We hold only the present time step in memory, calculating h(x) at that
+  ! time step by:
+  ! * calculating the difference dh(x) = h(x) - h(x-1)
+  ! * calculating the new value of h(x)
+
   do t = 1, time_steps
-    ! Calculate finite difference dh.
-    ! 1. Boundary condition on the left.
+    ! Boundary condition on the left: The grid is assumed to be connected,
+    ! i.e. When the blob leaves the grid at the right side, it enters
+    ! again at the left side.
     dh(1) = h(1) - h(grid_size)
-    ! 2. All other values.
+
     do concurrent (x = 2:grid_size)
       dh(x) = h(x) - h(x - 1)
     end do
 
-    ! Calculate h for this time-step based on dh.
     do concurrent (x = 1:grid_size)
       h(x) = h(x) - ((v * dh(x)) / dx) * dt
     end do
