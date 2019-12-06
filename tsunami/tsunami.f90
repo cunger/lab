3aee5e50fb9ede514c7f234b069743f525b22d99
Christina Unger <christina@mercury.ai>
2019-12-06 07:21:24 +0100

    [tsunami] change: physics (update height and velocity simultaneously)

--- tsunami/tsunami.f90
+++ tsunami/tsunami.f90
@@ -5,36 +5,17 @@ program tsunami
 
   implicit none
 
-  ! Discrete approximation of the advection equation with finite differences:
-  !
-  !    dh/dt + v*(dh/dt) = 0
-  !
-  ! Which, spelled out, corresponds to:
-  !
-  !    (h(x,t+1) - h(x,t))/dt + v*(h(x,t) - h(x-1,t))/dx = 0
-  !
-  ! Solving for the water height at the next time step, this gives:
-  !
-  !    h(x,t+1) = h(x,t) + v*(h(x,t) - h(x-1,t))/dx * dt
-  !
-  ! Where h(x,t) is the water height at the current time step,
-  ! (h(x,t) - h(x-1,t))/dx is the gradient, i.e. the rate of change in space,
-  ! multiplied by the velocity v, giving the rate of change in time.
-  !
-  ! We hold only the present time step in memory, calculating h(x) at that
-  ! time step by:
-  ! * calculating the difference dh(x) = h(x) - h(x-1)
-  ! * calculating the new value of h(x)
-
   integer, parameter :: grid_size = 100
-  integer, parameter :: time_steps = 100
+  integer, parameter :: time_steps = 5000
 
-  real, parameter :: dt = 1. ! time step [s]
-  real, parameter :: dx = 1. ! grid spacing [m]
-  real, parameter :: v  = 1. ! background flow speed [m/s]
+  real, parameter :: dt = 0.02 ! time step [s]
+  real, parameter :: dx = 1.   ! grid spacing [m]
+  real, parameter :: g  = 9.81 ! gravitational acceleration [m/s²]
+  real, parameter :: hmean = 10 ! ?
 
   integer :: x, t
 
+  real, dimension(grid_size) :: v  ! water velocity (for each point in the grid)
   real, dimension(grid_size) :: h  ! water height (for each point in the grid)
   real, dimension(grid_size) :: dh ! finite difference of water height
 
@@ -47,15 +28,17 @@ program tsunami
   if (grid_size < 1) stop 'grid_size must be >= 1'
   if (dt <= 0) stop 'time step dt must be > 0'
   if (dx <= 0) stop 'grid spacing dx must be > 0'
-  if (v  <= 0) stop 'background flow speed v must be > 0'
 
   call init_gaussian(h, 25, 0.02)
 
   print *, 0, h
 
+  v = 0
   do t = 1, time_steps
-    h = h - v * finite_difference(h) / dx * dt
-
+    v = v - (v * finite_difference(v) + g * finite_difference(h)) / dx * dt
+    h = h - finite_difference(v * (hmean + h)) / dx * dt
     ! print *, t, h
   end do
+
+  print *, t, h
 end program tsunami
